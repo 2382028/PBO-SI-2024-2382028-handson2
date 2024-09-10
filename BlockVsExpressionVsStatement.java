1c2d5aff1bd7b1cfbe734ffc8f40684f370f9d23
2382028 <2382028@unai.edu>
2024-09-10 10:13:48 +0700

    Handson 2

---
+++ BlockVsExpressionVsStatement.java
@@ -0,0 +1,12 @@
+public class BlockVsExpressionVsStatement {
+    public static void main(String[] args) {
+        //Ekspressi -> 10 + 10
+        //Ekspressi a = 20;
+        //Statement -> int a = 10 + 10;
+        int a = 10 + 10;
+        System.out.println("Daniella Sabathini R");
+        System.out.println("Daniella Sabathini R");
+        System.out.println("Daniella Sabathini R");
+        System.out.println("Daniella Sabathini R");
+    }
+}
---
+++ TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int NilaiLulusMinimum = 70;
+        int NilaiMahasiswa = 60;
+        String hasil = NilaiMahasiswa >= NilaiLulusMinimum ? "Lulus" : "Tidak Lulus";
+        System.out.println(hasil);
+    }
+}
